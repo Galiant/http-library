2cb47fcdcfa8aa7f51f81c7239afd85c55290650
Galiant <antonijodev@gmail.com>
2019-01-08 20:01:10 +0000

    POST method

--- easyhttp.js
+++ easyhttp.js
@@ -3,12 +3,12 @@ function easyHTTP() {
 }
 
 // Make an HTTP GET request
-easyHTTP.prototype.get = function(url, callback) {
+easyHTTP.prototype.get = function (url, callback) {
   this.http.open('GET', url, true);
 
   let self = this;
 
-  this.http.onload = function() {
+  this.http.onload = function () {
     if (self.http.status === 200) {
       callback(null, self.http.responseText);
     } else {
@@ -19,5 +19,16 @@ easyHTTP.prototype.get = function(url, callback) {
   this.http.send();
 };
 // Make an HTTP POST request
+easyHTTP.prototype.post = function (url, data, callback) {
+  this.http.open('POST', url, true);
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  let self = this;
+
+  this.http.onload = function () {
+    callback(null, self.http.responseText);
+  }
+  this.http.send(JSON.stringify(data));
+}
 // Make an HTTP PUT request
 // Make an HTTP DELETE request
